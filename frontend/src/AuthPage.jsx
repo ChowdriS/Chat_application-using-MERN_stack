9a09ce9a3b27fe96118c1c497f4b196188a223b5
ChowdriS <114232036+ChowdriS@users.noreply.github.com>
2023-09-18 15:46:30 +0530

    chatengine connection

--- frontend/src/AuthPage.jsx
+++ frontend/src/AuthPage.jsx
@@ -12,11 +12,9 @@ const AuthPage = (props) => {
 
   return (
     <div className="background">
-      <form onSubmit={onSubmit} className="form-card"> 
+      <form onSubmit={onSubmit} className="form-card">
         <div className="form-title">Welcome 👋</div>
-
         <div className="form-subtitle">Set a username to get started</div>
-
         <div className="auth">
           <div className="auth-label">Username</div>
           <input className="auth-input" name="username" />
